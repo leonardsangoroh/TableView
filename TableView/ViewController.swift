98f4883961fb3eabd13b594c5b0e1d46716d1f04
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-04-14 11:47:51 +0300

    chore: initialize tableview project

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Lee Sangoroh on 14/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
